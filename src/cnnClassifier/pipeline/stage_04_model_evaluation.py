7182157aaba89da556ca25752e03bb7d5eb42f52
patelshehbaz <patelshehbaz27@gmail.com>
2024-03-18 04:57:44 -0400

    evaluation

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -17,7 +17,7 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
 
 
 
@@ -32,4 +32,5 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+            
             
